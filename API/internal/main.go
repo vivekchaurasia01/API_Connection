e65958004f48675240cf58ac840f401bc9f70a0d
Vivek chaurasia <vivekkumarr221106@gmail.com>
2026-03-16 22:52:15 +0530

    writing Basic QueryParams

--- API/internal/main.go
+++ API/internal/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	// "API_CONNECTION/API/internal/users"
+	"bytes"
 	"log"
 	"log/slog"
 	"net/http"
+
+	"google.golang.org/genproto/googleapis/cloud/aiplatform/v1beta1/schema/predict/params"
 )
 
 // type UserData struct{
@@ -43,4 +46,23 @@ func handleGoodbye(w http.ResponseWriter, _*http.Request){
 		slog.Error("Error while Writing Resposes","error",err)
 	}
 }
-func 
+func handleHelloParameterize(w http.ResponseWriter, r *http.Request){
+	params := r.URL.Query()
+	username := "User"
+	userList := params["user"]
+	if len(userList) > 0 {
+		username = username[0]
+	}
+	handleHello(w,username)
+}
+func handleHello(w http.ResponseWriter, username string){
+	var output bytes.Buffer
+	output.WriteString("hello,")
+	output.WriteString("username")
+	output.WriteString("!\n")
+
+	if err != nil{
+		slog.Error("error writing resdponse body","err",err)
+		return
+	}
+}
