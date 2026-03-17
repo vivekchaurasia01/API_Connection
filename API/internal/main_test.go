62e826be7d83d31b9d56def1288220a6744fe172
Vivek chaurasia <vivekkumarr221106@gmail.com>
2026-03-17 22:53:26 +0530

    Writing first test

--- API/internal/main_test.go
+++ API/internal/main_test.go
@@ -1,7 +1,25 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
-func TestHandleHello(t *testing.T){
-	
+func TestHandleRoot(t *testing.T){
+	w := httptest.NewRecorder()
+
+	handleRoot(w,nil)
+
+	desireCode := http.StatusOK
+	if w.Code != desireCode{
+		t.Errorf("bad response code,expected :%v\n but got :%v\nbody:%s\n",desireCode,w.Code,w.Body.String())
+	}
+	expectedMessage := []byte("Welcome_User")
+	if !bytes.Equal(expectedMessage,w.Body.Bytes()){
+		t.Errorf("bad response code,Got :%q,Expected %q",w.Body.String(),expectedMessage)
+	}
+
+
 }
